3d6529fc05481cada987552cd929a8634564fcb5
Martin Legleiter <martin.legleiter@gmail.com>
2018-12-14 15:19:42 +0100

    Initial commit and first draft

---
+++ examples/Blink_AnalogRead/Blink_AnalogRead.ino
@@ -0,0 +1,78 @@
+#include <FreeRTOS.h>
+#include <task.h>
+
+/*-----------------------------------------------------------*/
+
+void vTaskBlink( void * pvParameters );
+void vTaskAnalogRead( void * pvParameters );
+
+/*-----------------------------------------------------------*/
+
+void setup( void )
+{
+    /* Initialize the serial port. */
+    Serial.begin( 9600 );
+    while( Serial != true );
+
+    /* Create our tasks. */
+    //xTaskCreate( vTaskBlink, "Blink", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
+    xTaskCreate(
+        vTaskBlink,                 /* Pointer to the Task implementation. */
+        "Blink",                    /* Internal name of the task. */
+        configMINIMAL_STACK_SIZE,   /* Wanted task stack size. */
+        NULL,                       /* Task parameter (optional). */
+        1,                          /* Task priority. */
+        NULL                        /* Task handle (optional). */
+                                                                                
+    );
+
+    xTaskCreate( vTaskAnalogRead, "Analog", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
+
+    /* At the end of the setup the kernel sheduler is started automatically. */
+}
+/*-----------------------------------------------------------*/
+
+void loop( void )
+{
+    /* The Arduino loop function is used as the idle hook. In FreeRTOSConfig.h
+    configUSE_IDLE_HOOK must be set because there are (serial) events that were
+    processed in the backround of the Arduino core implementation of loop(). */
+}
+/*-----------------------------------------------------------*/
+
+void vTaskBlink( void * pvParameters )
+{
+    /* Initialize the built in LED. */
+    pinMode( LED_BUILTIN, OUTPUT );
+
+    /* Keep the compiler happy because pvParameters is not used here. */
+    ( void ) pvParameters;
+
+    for( ;; )
+    {
+        /* Toggle the built in LED. */
+        digitalWrite( LED_BUILTIN, digitalRead( LED_BUILTIN ) ^ 1 );
+
+        /*  */
+        vTaskDelay( 1000 / portTICK_PERIOD_MS );
+    }
+}
+/*-----------------------------------------------------------*/
+
+void vTaskAnalogRead( void * pvParameters )
+{
+    int16_t sAnalogValue;
+
+    /* Keep the compiler happy because pvParameters is not used here. */
+    ( void ) pvParameters;
+
+    for( ;; )
+    {
+        /* Read the analog value from A0 print it out. */
+        sAnalogValue = analogRead( A0 );
+        Serial.println( sAnalogValue );
+
+        /* Short delay (1 tick) for stability. */
+        vTaskDelay( 1 );
+    }
+}
